fbdc209d1316450553b9914d108e1e8ec500212d
ygtw <jjlf23015888@gmail.com>
2013-08-08 16:08:50 +0800

    init

---
+++ my.php
@@ -0,0 +1,58 @@
+<?php
+ 
+/*替换为你自己的数据库名（可从管理中心查看到）*/
+$dbname = 'QjXFRjJkButACsxhqZGO';
+ 
+/*从环境变量里取出数据库连接需要的参数*/
+$host = getenv('HTTP_BAE_ENV_ADDR_SQL_IP');
+$port = getenv('HTTP_BAE_ENV_ADDR_SQL_PORT');
+$user = getenv('HTTP_BAE_ENV_AK');
+$pwd = getenv('HTTP_BAE_ENV_SK');
+ 
+/*接着调用mysql_connect()连接服务器*/
+$link = @mysql_connect("{$host}:{$port}",$user,$pwd,true);
+if(!$link) {
+    die("Connect Server Failed: " . mysql_error());
+}
+/*连接成功后立即调用mysql_select_db()选中需要连接的数据库*/
+if(!mysql_select_db($dbname,$link)) {
+    die("Select Database Failed: " . mysql_error($link));
+}
+ 
+
+
+/*至此连接已完全建立，就可对当前数据库进行相应的操作了*/
+/*！！！注意，无法再通过本次连接调用mysql_select_db来切换到其它数据库了！！！*/
+/* 需要再连接其它数据库，请再使用mysql_connect+mysql_select_db启动另一个连接*/
+
+$sql = "UPDATE  `QjXFRjJkButACsxhqZGO`.`personal` SET  `head` =   '$_GET[head]' , `chest`='$_GET[chest]'  , `chest`='$_GET[chest]'  , `chest`='$_GET[chest]'   , `belly`='$_GET[belly]'  , `hand`='$_GET[hand]' , `leg`='$_GET[leg]' WHERE  `personal`.`id` =1;";
+	$ret = mysql_query($sql, $link);
+	if ($ret === false) {
+		die("Insert Failed: " . mysql_error($link));
+	} else {
+		echo "Insert Succeed<br />";
+	}
+
+
+
+
+	$sql = "SELECT * FROM  `personal` LIMIT 0 , 30";
+	$ret = mysql_query($sql, $link);
+	if ($ret === false) {
+		die("Select Failed: " . mysql_error($link));
+	} else {
+		echo "Select Succeed<br />";
+		while ($row = mysql_fetch_assoc($ret)) {
+			echo "{$row['head']} {$row['leg']} {$row['chest']} {$row['belly']}{$row['hand']}  {$row['leg']}<br />";
+		}
+	}
+	
+/**
+ * 接下来就可以使用其它标准php mysql函数操作进行数据库操作
+ */
+ 
+/*显式关闭连接，非必须*/
+mysql_close($link);
+
+
+?>
